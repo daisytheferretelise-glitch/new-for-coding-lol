3af9318f2abf19fd6066c3af5446660b45646900
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-12-06 16:24:58 +0000

    operatores

---
+++ operatores.py
@@ -0,0 +1,20 @@
+a=100
+b=1
+c=a+b
+print("John acidentaly used a AK-47 rifle on his exam papers help him find the result")
+result=a+b+c
+print("The result is:", result)
+if result>120:
+    print("WOW! John passed with flying colors! But i think guns aren't allowed in exams so please dont next time John")
+else:
+    print("Oh no! John failed his exam! But if he studied instead of playing with guns he would pass.But im callling the cops on him for bringing a gun to school")
+a=10
+b=10
+c=a+b
+print(" Mrs John acidentaly used a rifle on his exam papers help him find the result")
+result=a+b+c
+print("The result is:", result)
+if result>120:
+    print("WOW!  Mrs John passed with flying colors! But i think guns aren't allowed in exams so please dont next time  Mrs John")
+else:
+    print("Oh no!  Mrs John failed his exam! But if he studied instead of playing with guns he would pass.But im callling the cops on her for bringing a gun to school")
