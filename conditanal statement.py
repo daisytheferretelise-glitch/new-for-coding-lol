61b4057c11b459149dc6ae01da7978dee038c524
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-12-12 18:48:26 +0000

    conditanal staement

---
+++ conditanal statement.py
@@ -0,0 +1,13 @@
+print("john dosent know if he sick enough so help him and find out his attendece")
+medical_cause=input("John do you have a medical cause  Y or N:")
+attendance=int(input("enter attendance of John"))
+if medical_cause == "Y":
+   print("you are very sick but you can do exams")
+else:
+ 
+  
+ if attendance>=75:
+  print("you are allowed to go to school and comunicate with your friends")
+
+ else:
+   print("you need a docor youcant sit in the exam hall")
--- number swap.py
+++ number swap.py
@@ -2,4 +2,4 @@ a, b, c = 10, 20, 30
 print("Before swapping: a =", a, ", b =", b, ", c =", c)
 # Swap values
 c, a, b = a, b, c
-print("After swapping: a =", a, ", b =", b, ", c =", c)
+print("After swapping: a =", a, ", b =", b, ", c =", c) 
