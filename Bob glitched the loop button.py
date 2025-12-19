a6ed54ee72f536f059810645afdc8d3f95da29b1
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-12-19 18:40:38 +0000

    LOOP

---
+++ Bob glitched the loop button.py
@@ -0,0 +1,4 @@
+five=-100
+not_gonna_help_you=1000
+while five <= not_gonna_help_you:
+    print("YOU FAILED HAHAHAHAHAHHAHAAHAHHAHA")
---
+++ finding the sum of natural numbers.py
@@ -0,0 +1,9 @@
+num=int(input("Enter value or terms: "))
+
+sum=0
+i=1
+
+while i <= num:
+    sum = sum + i
+    i = i+1
+print ("The sum of the actual number is: " ,sum)
