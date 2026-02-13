037611320071e744dba9c3c247aeb78669b367fc
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2026-02-13 19:07:28 +0000

    list

---
+++ Bob has a list.py
@@ -0,0 +1,14 @@
+print("Bobs wife has given bob chores her name is Bobeista")
+empty_list=[]
+print(empty_list)
+print("right now there are 0 chores")
+numbers=[1,2,3,4,5]
+print("Bobeista says Bob has to do this amount of chores",numbers)
+
+triple=[1,2,3]*3
+print("Bob dosent's so there are more chores now",triple)
+
+alist=[100,200,300,400,500]
+alist=alist[::-1]
+
+print(alist, "\n")
