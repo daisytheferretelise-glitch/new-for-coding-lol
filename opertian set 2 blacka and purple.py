675fff829ca16c3265b8d691756a58a97cfcc993
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2026-02-25 19:53:11 +0000

    SETS

---
+++ opertian set 2 blacka and purple.py
@@ -0,0 +1,9 @@
+setx={"black","purple"}
+sety={"orange","purple"}
+print("oringal set elemental: ")
+print(setx)
+print(sety)
+
+print("\nIntersection of two sets: ")
+setz=setx.intersection(sety)
+print(setz)
