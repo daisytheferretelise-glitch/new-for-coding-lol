ca3bc93e996062d1bd4c5da9769a6257c60c4231
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2026-06-05 20:01:37 +0100

    no

---
+++ peppa pig.py
@@ -0,0 +1,21 @@
+from tkinter import*
+
+
+window= Tk()
+window.title("Event Handler")
+window.geometry("500x600")
+
+def handle_keypress(event):
+    print(event.char)
+
+window.bind("<Key>",handle_keypress)   
+
+def handle_click(event):
+    print("\nThe Bob the XII junior prince was crowned king!")
+
+button=Button(text="very confidental im not joking")
+button.pack()
+
+button.bind("<Button-1>",handle_click)
+
+window.mainloop()
